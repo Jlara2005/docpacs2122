9153d6b5e118f805bd4b2d951ea188fbcdaffa91
Jlara2005 <90395076+Jlara2005@users.noreply.github.com>
2022-03-21 11:17:20 -0400

    cs

--- nocti_prep/JasonLara_pogication/app.js
+++ nocti_prep/JasonLara_pogication/app.js
@@ -1,7 +1,48 @@
-const app = require('express');
+const express = require('express');
 const ejs = require('ejs');
 const fs = require('fs');
+var app = express()
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+
+studentData = JSON.parse(fs.readFileSync("data.json"))
+class Student{
+  constructor(name,seqeunce){
+    this.name = name;
+    this.id = sequence;
+    this.balance = 0
+  }
+}
+
+function find(){
+  for (var student in studentid) {
+    if (object.hasOwnProperty(variable)) {
+
+    }
+  }
+}
+app.get("/view",function(req,res){
+  if (req.query.studentid) {
+
+  }
+  res.render("view",{
 
 
 
-app.use(express.urlencoded({extended: True}))
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8000)
