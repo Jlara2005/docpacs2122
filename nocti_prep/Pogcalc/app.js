8301e5a9aa1d39329517e0072bd4039fcaaedfcf
Jlara2005 <90395076+Jlara2005@users.noreply.github.com>
2022-03-24 14:31:45 -0400

    Update app.js

--- nocti_prep/Pogcalc/app.js
+++ nocti_prep/Pogcalc/app.js
@@ -6,8 +6,32 @@ app.set("view engine","ejs")
 app.use(express.urlencoded({extended: true}))
 //start
 
+
+var loser = []
+var winner = []
+var pogs = 0
+var averagepogs = 0
+var timelapse = []
+var averagetimelapse = 0
+var totaltimelapse = 0
+var winners2000 = []
+
+
 var champs = JSON.parse(fs.readFileSync('Pogchamps.json'))
 champs.forEach((champ, i) => {
-  console.log(champ.lastWin);
+
+    timelapse[i] = Date.now() - champ.lastWin;
+    totaltimelapse += timelapse[i]
+
+    if (timelapse < 631152000000 ) {
+      winner.push(champ.name)
+    } else {
+      loser.push(champ.name)
+    }
+    pogs += champ.pogsOwned
+    if (timelapse < 946688400000) {
+      winners2000.push(champ.name)
+    }
+    console.log(loser);
 
 });
